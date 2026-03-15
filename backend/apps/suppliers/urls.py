7ac720562b2a0c40cd9bd749edd876b3fa4da1ff
bkam13-dev <angemichaelboni10@gmail.com>
2026-03-15 01:51:11 +0000

    feat(backend): gestion des permissions et des accès , ajout de la connexion par google

--- backend/apps/suppliers/urls.py
+++ backend/apps/suppliers/urls.py
@@ -2,8 +2,8 @@ from django.urls import path, include
 from apps.suppliers.views import CreateSupplierView, ListSupplierView, DestroySupplierView, RetrieveSupplierView
 
 urlpatterns = [
-    path('create/', CreateSupplierView.as_view()),
+    path('/', ListSupplierView.as_view()),
+    path('new/', CreateSupplierView.as_view()),
     path('<uuid:pk>/', RetrieveSupplierView.as_view()),
-    path('<uuid:pk>/delete/', DestroySupplierView.as_view()),
-    path('list/', ListSupplierView.as_view()),
+    path('<uuid:pk>/', DestroySupplierView.as_view()),
 ]
--- backend/apps/suppliers/views.py
+++ backend/apps/suppliers/views.py
@@ -2,24 +2,29 @@ from django.shortcuts import render
 from rest_framework import generics
 from apps.suppliers.serializers import SupplierSerializers
 from apps.suppliers.models import Supplier
+from rest_framework import permissions
+from core.permissions import IsAdmin, IsManager, IsStaff
 
-# Create your views here.
 
 class CreateSupplierView(generics.CreateAPIView):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializers
+    permission_classes = [permissions.IsAuthenticated, IsManager]
     
 class ListSupplierView(generics.ListAPIView):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializers
-    
+    permission_classes = [permissions.IsAuthenticated, IsStaff]
+  
 class RetrieveSupplierView(generics.RetrieveAPIView):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializers
-    
+    permission_classes = [permissions.IsAuthenticated, IsStaff]
+   
 class DestroySupplierView(generics.DestroyAPIView):
     queryset = Supplier.objects.all()
     serializer_class = SupplierSerializers
-    
+    permission_classes = [permissions.IsAuthenticated, IsManager]
+
 
     
